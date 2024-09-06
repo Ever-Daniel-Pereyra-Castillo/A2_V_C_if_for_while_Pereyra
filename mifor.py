2e208c01c993e3f6804ff1b02ebe80b7d95e78ea
Ever-Daniel-Pereyra-Castillo <a.22308051281282@cbtis128.edu.mx>
2024-09-06 10:19:41 -0600

    Add files via upload

---
+++ mifor.py
@@ -0,0 +1,7 @@
+edad = 21
+print("Necesitas tener 18 años para entrar a esta pagina")
+
+if edad >= 18:
+    print("BIENVENIDO")
+else:
+    print("no puedes pasar :(") 
---
+++ miwhile.py
@@ -0,0 +1,4 @@
+x = 22
+while x < 22:
+  print(x)
+  x += 2
